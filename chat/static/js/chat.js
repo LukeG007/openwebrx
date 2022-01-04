6006e29994a33894062efe1a317f7612bff5955e
LukeG007 <luke@gilsinger.net>
2022-01-03 19:03:35 -0500

    Fix msg box and scroll

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -5,13 +5,14 @@ socket.on("msg", function (data) {
     msg = document.createElement("li");
     msg.innerText = data['username'] + ': ' + data['msg'];
     chat.appendChild(msg);
+    window.scrollTo(0,document.body.scrollHeight);
 });
 
 function send() {
     msgbox = document.getElementById("msg");
     socket.emit("msg", {"msg": msgbox.value, "username": username})
     msgbox.value = "";
-}
+};
 
 document.getElementById('msg').onkeydown = function(e){
     if(e.keyCode == 13){
